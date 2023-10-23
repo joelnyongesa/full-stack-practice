71eaae3947f57b0d43a3296023d0506642678110
joelnyongesa <joelnyongesa148@gmail.com>
2023-10-24 00:18:14 +0300

    refactor: minor bugs and fixes

--- server/app.py
+++ server/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from models import db, User, Post
 
 app = Flask(__name__)
 
 api=Api(app)
+
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
